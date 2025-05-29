3f63842ba18daae0c923ae765919e7a0f1aa78fd
andrec81 <80151792+andrec81@users.noreply.github.com>
2025-05-29 10:22:39 +0200

    Added blank page

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 // Import your screens
 import Login from '../screens/Login';
 import Register from '../screens/Register';
+import Blank from '../screens/Blank';
 
 
 const Stack = createStackNavigator();
@@ -14,6 +15,7 @@ function AppNavigator()
 		<Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false, animationEnabled: false }}>
 			<Stack.Screen name="Login" component={Login} />
 			<Stack.Screen name="Register" component={Register} />
+			<Stack.Screen name="Blank" component={Blank} />
 		</Stack.Navigator>
 	);
 }
